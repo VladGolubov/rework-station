965900b4f09fc12ae92be2f3af009b4afe232e4b
VladGolubov <vladgolubov@outlook.com>
2020-03-30 18:04:35 +0300

    Всё прекрасно заработало, неоюходимо откалибровать фен и нижний подогрев, переписать на ООП или хотя бы переписать через перегружаемые функции. Также возможно уменьшить размеры платы.
    Signed-off-by: VladGolubov <vladgolubov@outlook.com>

--- reworkstation.ino
+++ reworkstation.ino
@@ -1,355 +1,153 @@
- /*   --------------------------------------------------
-  * | Имя    : Паяльная станция                          |
-  * | Автор  : Влад Голубов                              |
-  * | Дата   :  05.01.2020                               |
-  * | Версия :  2.0.2                                    |
-  *   --------------------------------------------------
-  * Добавлено:
-  * 1.PID регулятор 
-  * 2.Нижний подогрев
-  * Функции:
-  * 1. Проверка температуры термопарой(терморезистором) на паяльнике(s_thermPin), 
-  *    если температура меньше установленной резистором (s_res), 
-  *    то включается высокий сигнал на выводе(s_ten) 
-  * 2. Проверка температуры термопарой(терморезистором) на фене(f_thermPin), 
-  *    если температура меньше установленной резистором (f_res), 
-  *    то включается высокий сигнал на выводе(f_ten) 
-  * 3. Если темература фена(f_thermPin) > минимальной температуры(f_fanAlarm), 
-  *    то на вывод (f_fan) включается высокий сигнал
-  * Материалы: 
-  * 
- */
+/*   --------------------------------------------------
+   | Имя    : Паяльная станция                          |
+   | Автор  : Влад Голубов                              |
+   | Дата   :  05.01.2020                               |
+   | Версия :  2.0.2                                    |
+     --------------------------------------------------
+   Добавлено:
+   1.PID регулятор
+   2.Нижний подогрев
+   Функции:
+   1. Проверка температуры термопарой(терморезистором) на паяльнике(s_thermPin),
+      если температура меньше установленной резистором (s_res),
+      то включается высокий сигнал на выводе(s_ten)
+   2. Проверка температуры термопарой(терморезистором) на фене(f_thermPin),
+      если температура меньше установленной резистором (f_res),
+      то включается высокий сигнал на выводе(f_ten)
+   3. Если темература фена(f_thermPin) > минимальной температуры(f_fanAlarm),
+      то на вывод (f_fan) включается высокий сигнал
+   Материалы:
 
-/*#include      <max6675.h>                            // Подключаем библиотеку max6675 для работы с датчиком температуры 
-const uint8_t thermoDO  = 4;                           // Определяем константу с указанием № вывода Arduino к которому подключён вывод DO  ( SO, MISO ) модуля на чипе MAX6675
-const uint8_t thermoCS  = 5;                           // Определяем константу с указанием № вывода Arduino к которому подключён вывод CS  ( SS )       модуля на чипе MAX6675
-const uint8_t thermoCLK = 6;                           // Определяем константу с указанием № вывода Arduino к которому подключён вывод CLK ( SCK )      модуля на чипе MAX6675
-MAX6675       thermoco(thermoCLK, thermoCS, thermoDO); // Объявляем объект thermo для работы с функциями и методами библиотеки max6675, указывая выводы ( CLK , CS , DO )
-не знаю заработакт или нет*/
-//----------------------------------------------------------------------------------------------
-
- #include <SPI.h> 
- #include <PIDController.h>
- #include <Wire.h> 
- #include <LiquidCrystal_I2C.h> 
-
-//----------------------------------------------------------------------------------------------
- 
-
- LiquidCrystal_I2C lcd(0x27,16,2);
- /*Константы*/
- #define max_temp = 500
- #define f_fanAlarm 50
- #define alarm_temp 550
- #define error_temp 0 
- /*
-#define B 3950 // B-коэффициент
-#define SERIAL_R 220 // сопротивление последовательного резистора, 220 Ом
-#define THERMISTOR_R 60 // номинальное сопротивления термистора, 60 Ом
-#define NOMINAL_T 25 // номинальная температура (при которой TR = 60 Ом)
 */
 
- /*Аналоговые входы*/
- //#define dh_thermPin A4   PC4 
- //#define f_thermPin A2    PC2
- //#define s_thermPin A3    PC3
- #define s_res A1         //PC1
- #define f_res A0         //PC0
- #define dh_res A2        //PC5 
- 
- /*Цифровые входы*/
- int16_t  s_ten = 9;   //PB1
- int16_t  f_ten = 10;  //PB2 поменять на 8
- int16_t  dh_ten = 11; //PB4, перепаять с вентилятора на нижний подогрев!
- int16_t  f_fan = 6;   //PB3
- int16_t  SW1 = 8;     //PD6  паяльник
- int16_t  SW2 = 7;     //PD7  фен
- int16_t  SW3 = 2;     //PB0  геркон 
- int16_t  SW4 = 3;     //PB5  нижний подогрев
- 
- /*Переменнные*/
- int16_t dh_thermo;
- int16_t f_thermo;
- int16_t s_thermo;
- int16_t s_resd;
- int16_t f_resd;
- int16_t dh_resd;
-//float steinhart; если используем терморезистор, то разкомментировать
-//float tr;        и перепаять на плате 
+#include      <max6675.h>                            // Подключаем библиотеку max6675 для работы с датчиком температуры 
+const int miso         = 12;                           // Определяем константу с указанием № вывода Arduino к которому подключён вывод DO  ( SO, MISO ) модуля на чипе MAX6675
+const int sck          = 13;                          // Определяем константу с указанием № вывода Arduino к которому подключён вывод CS  ( SS )       модуля на чипе MAX6675
+const byte max6675_num = 3;
+const int max6675_cs_pins[max6675_num] = {1, 4, 5};
 
-PIDController pid;
+MAX6675   max6675s[max6675_num] = {
+  MAX6675(sck, max6675_cs_pins[0], miso),
+  MAX6675(sck, max6675_cs_pins[1], miso),
+  MAX6675(sck, max6675_cs_pins[2], miso),
+};
+
+//----------------------------------------------------------------------------------------------
+
+
+#include <PIDController.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+//----------------------------------------------------------------------------------------------
+
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+/*Константы*/
+#define max_temp = 500
+#define f_fanAlarm 50
+#define alarm_temp 550
+#define error_temp 0
+/*
+  #define B 3950 // B-коэффициент
+  #define SERIAL_R 220 // сопротивление последовательного резистора, 220 Ом
+  #define THERMISTOR_R 60 // номинальное сопротивления термистора, 60 Ом
+  #define NOMINAL_T 25 // номинальная температура (при которой TR = 60 Ом)
+*/
+
+/*Аналоговые входы*/
+//#define dh_thermPin A4   PC4
+//#define f_thermPin A2    PC2
+//#define s_thermPin A3    PC3
+#define s_res A1         //PC1
+#define f_res A0         //PC0
+#define dh_res A2        //PC5 
+
+/*Цифровые входы*/
+int16_t  s_ten = 9;   //PB1
+int16_t  f_ten = 10;  //PB2 поменять на 8
+int16_t  dh_ten = 11; //PB4, перепаять с вентилятора на нижний подогрев!
+int16_t  f_fan = 6;   //PB3
+int16_t  SW1 = 8;     //PD6  паяльник
+int16_t  SW2 = 7;     //PD7  фен
+int16_t  SW3 = 2;     //PB0  геркон
+int16_t  SW4 = 3;     //PB5  нижний подогрев
+
+/*Переменнные*/
+int16_t dh_thermo;
+int16_t f_thermo;
+int16_t s_thermo;
+int16_t s_resd;
+int16_t f_resd;
+int16_t dh_resd;
+//float steinhart; если используем терморезистор, то разкомментировать
+//float tr;        и перепаять на плате
+
+uint32_t timing1, timing2, timing3;
+
+
 PIDController pid_solder;
 PIDController pid_fen;
 PIDController pid_dh;
 
 
-byte degree[8] = // кодируем символ градуса
+byte degree[8] = // кодируем 1 символ градуса
 {
-B00111,
-B00101,
-B00111,
-B00000,
-B00000,
-B00000,
-B00000,
+  B00111,
+  B00101,
+  B00111,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
 };
-byte degree2[8] = // кодируем символ градуса
+byte degree2[8] = // кодируем 2 символ градуса
 {
-B00111,
-B00101,
-B00111,
-B01100,
-B10000,
-B10000,
-B01100,
+  B00111,
+  B00101,
+  B00111,
+  B01100,
+  B10000,
+  B10000,
+  B01100,
 };
-/*рабочий вариант отображения
 
-  lcd.setCursor(0, 0);
-    lcd.print("Solder:");
-  if (digitalRead(SW1) == HIGH){
-    //delay(200);
-    lcd.print(s_thermo);
-    lcd.print(" ");
-    lcd.setCursor(12, 0);
-    lcd.print(s_resd);
-    lcd.print(" ");
-  }
-  else{
-   
-    lcd.print(" Off");
-  }
-  // Данные фена на дисплей
-  lcd.setCursor(0, 1);
-  lcd.print("Fen:");
-  lcd.setCursor(4, 1);
-  if (digitalRead(SW2) == HIGH){
-// delay(200);
-    lcd.print(f_thermo);
-    lcd.print(" ");
-    lcd.setCursor(9, 1);
-    lcd.print(f_resd);
-    lcd.print(" ");
-  }
- else{
-  
-    lcd.print(" Off");
-  }
-  delay(300);
-
-lcd.clear();
-*/
 /*Рабочий вариант для терморезистора
- * float tr = 1023.0 / s_thermo - 1;
-    tr = SERIAL_R / tr;  
+   float tr = 1023.0 / s_thermo - 1;
+    tr = SERIAL_R / tr;
     float steinhart;
     steinhart = tr / THERMISTOR_R; // (R/Ro)
     steinhart = log(steinhart); // ln(R/Ro)
     steinhart /= B; // 1/B * ln(R/Ro)
     steinhart += 1.0 / (NOMINAL_T + 273.15); // + (1/To)
     steinhart = 1.0 / steinhart; // Invert
-    steinhart -= 273.15; 
+    steinhart -= 273.15;
   steinhart = s_thermo;*/
 
-void control_solder(){
- if (digitalRead(SW1)== HIGH ){
-  s_resd = analogRead(s_res);
-   s_resd = map(s_resd, 0, 1024, 0, 500);
-    s_resd = constrain(s_resd, 0, 500);
- /* if(s_thermo < s_resd){
-    digitalWrite(s_ten, HIGH);
-    delay(50);
-  }else{
-     digitalWrite(s_ten, LOW);
-  } рабочий кусок кода
- }*/
-    pid_solder.setpoint(s_resd);   
-    int16_t output1 = pid_solder.compute(s_thermo);
-    analogWrite(s_ten, output1);
-    if(s_thermo > alarm_temp ){
-      
-    }
-   digitalWrite(s_ten, LOW);
- }
-};
 
-void control_fen(){
- if (digitalRead(SW2)== HIGH && digitalRead(SW3)== LOW ){
-  f_resd = analogRead(f_res);
-   f_resd = map(f_resd, 0, 1024, 0, 500);
-    f_resd = constrain(f_resd, 0, 500);
- /* if(f_thermo < f_resd){
-    digitalWrite(f_ten, HIGH);
-    delay(50);
-  }else{
-     digitalWrite(f_ten, LOW);
-  }
- }
-*/
-     pid_fen.setpoint(f_resd);   
-     int16_t output2 = pid_fen.compute(f_thermo);
-     analogWrite(f_ten, output2);
-      if (f_thermo >  f_fanAlarm  || digitalRead(SW3) == HIGH ){
-         digitalWrite(f_fan, HIGH);}
-      else{ 
-         digitalWrite(f_fan, LOW);} 
-     if (f_thermo > alarm_temp ){
-         digitalWrite(f_ten, LOW);
-   }
- }
-};
-
-void control_dh(){
- if (digitalRead(SW4)== HIGH ){
-  dh_resd = analogRead(dh_res);
-   dh_resd = map(dh_resd, 0, 1024, 0, 500);
-    dh_resd = constrain(dh_resd, 0, 500);
- /* if(dh_thermo < dh_resd){
-    digitalWrite(dh_ten, HIGH);
-    delay(50);
-  }else{
-     digitalWrite(dh_ten, LOW);
-  } рабочий кусок кода
- }*/
-     pid_dh.setpoint(dh_resd);   
-     int16_t output3 = pid_dh.compute(dh_thermo);
-     analogWrite(dh_ten, output3);
-      if (dh_thermo > alarm_temp ){
-         digitalWrite(s_ten, LOW);
-       }
-   }
-}; 
-
-void display_lcd(){
- //Solder
- lcd.setCursor(0,0);
-    lcd.print("S:");
-    lcd.setCursor(0,1);
-    lcd.print("H:"); 
-  
-if(digitalRead(SW1) == HIGH){  
-  lcd.setCursor(2,0);
-   lcd.print(s_thermo);
-   //delay(10);
-  lcd.setCursor(2,1);
-   lcd.print(s_res);
- //  delay(10);
-   }else{
-    if(s_thermo == error_temp && alarm_temp){  
-    lcd.setCursor(2,0);
-    lcd.print("Err");
-    lcd.setCursor(2,1);
-    lcd.print("Err");
-  }else{
-  lcd.setCursor(2,0);
-    lcd.print("off");
-    lcd.setCursor(2,1);
-    lcd.print("off");
-  }
-   }
- //Fen  
-    lcd.setCursor(5,0);
-    lcd.print("F:");
-    lcd.setCursor(5,1);
-    lcd.print("H:");
-if(digitalRead(SW2) == HIGH){
-  lcd.setCursor(7,0);
-  lcd.print(f_thermo);
- // delay(10);
-  lcd.setCursor(7,1);
-  lcd.print(f_res);
-//  delay(10);
-}else{
-  if(f_thermo == error_temp  && alarm_temp){  
-    lcd.setCursor(7,0);
-    lcd.print("Err");
-    lcd.setCursor(7,1);
-    lcd.print("Err");
-  }else{
-    lcd.setCursor(7,0);
-    lcd.print("off");
-    lcd.setCursor(7,1);
-    lcd.print("off");
-  }
-}
-
- //Downheating
-   lcd.setCursor(15,0);
-  lcd.print("\2C");
-   lcd.setCursor(15,1);
-  lcd.print("\1C");
-  lcd.setCursor(10,0);
-    lcd.print("D:");
-    lcd.setCursor(10,1);
-    lcd.print("H:"); 
- if(digitalRead(SW4) == HIGH){
-  lcd.setCursor(12,0);
-   lcd.print(dh_thermo);
- //  delay(10);
-  lcd.setCursor(12,1);
-   lcd.print(dh_res);
- //  delay(10);
- }else{
-  if(dh_thermo == error_temp && alarm_temp){  
-    lcd.setCursor(12,0);
-    lcd.print("Err");
-    lcd.setCursor(12,1);
-    lcd.print("Err");
-  }else{
-   lcd.setCursor(12,0);
-    lcd.print("off");
-    lcd.setCursor(12,1);
-    lcd.print("off"); 
-  //Downheating
-  }
- }
-}
-
-
-double readCelsius(uint8_t cs) { 
- uint16_t v; 
-
- digitalWrite(cs, LOW); 
- v = SPI.transfer(0x00); 
- v <<= 8; 
- v |= SPI.transfer(0x00); 
- digitalWrite(cs, HIGH); 
-
- if (v & 0x4) { 
-  // uh oh, no thermocouple attached! 
-  return NAN; 
- } 
-
- v >>= 3; 
-
- return v*0.25; 
-} 
- 
 void setup() {
-  SPI.begin();
- Serial.end();
+
+  Serial.end();
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("light_side v1.2");
-  lcd.setCursor(0,1);
-  lcd.print("Rework Station");
-  delay(1000);
-  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Rework");
+  lcd.setCursor(0, 1);
+  lcd.print("Station");
 
-   lcd.createChar(1, degree); // Создаем символ под номером 1
-   lcd.createChar(2, degree2); // Создаем символ под номером 1
-  
-   pid_solder.begin(); 
-   pid_fen.begin();   
-   pid_dh.begin();      
-   pid_dh.tune(1, 1, 1);    
-   pid_dh.limit(0, 500);    
-   pid_fen.tune(1, 1, 1);    
-   pid_fen.limit(0, 500);   
-   pid_solder.tune(1, 1, 1); // Tune the PID, arguments: kP, kI, kD
-   pid_solder.limit(0, 500); // Limit the PID output between 0 and 255, t
- 
+  lcd.createChar(1, degree); // Создаем символ под номером 1
+  lcd.createChar(2, degree2); // Создаем символ под номером 1
+
+  pid_solder.begin();
+  pid_fen.begin();
+  pid_dh.begin();
+  pid_dh.tune(1, 1, 1);
+  pid_dh.limit(0, 255);
+  pid_fen.tune(1, 1, 1);
+  pid_fen.limit(0, 255);
+  pid_solder.tune(75, 0.07, 0.2); // Tune the PID, arguments: kP, kI, kD
+  pid_solder.limit(0, 255); // Limit the PID output between 0 and 255, t
+
   pinMode(f_fan, OUTPUT);
   pinMode(s_ten, OUTPUT);
   pinMode(f_ten, OUTPUT);
@@ -364,30 +162,188 @@ void setup() {
   pinMode(SW2, INPUT);
   pinMode(SW3, INPUT);
   pinMode(SW4, INPUT);
- pinMode(1, OUTPUT); 
- pinMode(4, OUTPUT); 
- pinMode(5, OUTPUT); 
- digitalWrite(1, HIGH); 
- digitalWrite(4, HIGH); 
- digitalWrite(5, HIGH); 
+  // pinMode(1, OUTPUT);
+  // pinMode(4, OUTPUT);
+  // pinMode(5, OUTPUT);
+  lcd.clear();
+  delay(500);
 
 }
 
 
 void loop() {
-  
-   s_thermo = readCelsius(1);
-   f_thermo = readCelsius(4);
-   dh_thermo = readCelsius(5);
 
-   analogRead(s_res);
-   analogRead(f_res);
-   analogRead(dh_res);
- control_solder();
- control_dh();
- control_fen();
- display_lcd();
 
- }
-  
-  
+  //void control_solder(){
+  if (digitalRead(SW1) == HIGH ) {
+    s_resd = analogRead(s_res);
+    s_resd = map(s_resd, 0, 1024, 0, 500);
+    s_resd = constrain(s_resd, 0, 500);
+    if (millis() - timing2 > 200) {
+      timing2 = millis();
+    s_thermo = max6675s[0].readCelsius();
+       s_thermo = s_thermo / 2;
+    };
+ //  if (millis() - timing3 > 100) {
+   //   timing3 = millis();
+    if (s_thermo < s_resd) {
+      digitalWrite(s_ten, HIGH);
+    } else {
+      digitalWrite(s_ten, LOW);
+    };
+ 
+      /*    pid_solder.setpoint(s_resd);
+          int16_t output1 = pid_solder.compute(s_thermo);
+          analogWrite(s_ten, output1);
+        }; */
+      if (s_thermo == alarm_temp ) {
+        digitalWrite(s_ten, LOW);
+      };
+    };
+  //};
+
+  //void control_fen(){
+  if (digitalRead(SW2) == HIGH && digitalRead(SW3) == LOW ) {
+    f_resd = analogRead(f_res);
+    f_resd = map(f_resd, 0, 1024, 0, 500);
+    f_resd = constrain(f_resd, 0, 500);
+    f_thermo = max6675s[1].readCelsius();
+
+    /* if(f_thermo < f_resd){
+       digitalWrite(f_ten, HIGH);
+       delay(50);
+      }else{
+        digitalWrite(f_ten, LOW);
+      }
+    */
+    pid_fen.setpoint(f_resd);
+    int16_t output2 = pid_fen.compute(f_thermo);
+    analogWrite(f_ten, output2);
+    if (f_thermo >  f_fanAlarm  || digitalRead(SW3) == HIGH ) {
+      digitalWrite(f_fan, HIGH);
+    }
+    else {
+      digitalWrite(f_fan, LOW);
+    }
+    if (f_thermo == alarm_temp ) {
+      digitalWrite(f_ten, LOW);
+    }
+  };
+  //};
+
+  //void control_dh(){
+  if (digitalRead(SW4) == HIGH ) {
+    dh_resd = analogRead(dh_res);
+    dh_resd = map(dh_resd, 0, 1024, 0, 500);
+    dh_resd = constrain(dh_resd, 0, 500);
+    dh_thermo = max6675s[2].readCelsius();
+
+    /* if(dh_thermo < dh_resd){
+       digitalWrite(dh_ten, HIGH);
+       delay(50);
+      }else{
+        digitalWrite(dh_ten, LOW);
+      } рабочий кусок кода
+      }*/
+    pid_dh.setpoint(dh_resd);
+    int16_t output3 = pid_dh.compute(dh_thermo);
+    analogWrite(dh_ten, output3);
+    if (dh_thermo > alarm_temp ) {
+      digitalWrite(s_ten, LOW);
+    }
+    if (dh_thermo == alarm_temp ) {
+      digitalWrite(dh_ten, LOW);
+    };
+
+  };
+  //};
+
+  if (millis() - timing1 > 300) {
+    timing1 = millis();
+    lcd.clear();
+    //Solder
+    lcd.setCursor(0, 0);
+    lcd.print("S:");
+    lcd.setCursor(0, 1);
+    lcd.print("H:");
+
+    if (digitalRead(SW1) == HIGH) {
+      lcd.setCursor(2, 0);
+      lcd.print( s_thermo );
+      //delay(10);
+      lcd.setCursor(2, 1);
+      lcd.print(  s_resd);
+      //  delay(10);
+    } else {
+      lcd.setCursor(2, 0);
+      lcd.print("off");
+      lcd.setCursor(2, 1);
+      lcd.print("off");
+    };
+    if (s_thermo  == error_temp && alarm_temp) {
+      lcd.setCursor(2, 0);
+      lcd.print("Err");
+      lcd.setCursor(2, 1);
+      lcd.print("Err");
+    };
+
+    //Fen
+    lcd.setCursor(5, 0);
+    lcd.print("F:");
+    lcd.setCursor(5, 1);
+    lcd.print("H:");
+    if (digitalRead(SW2) == HIGH) {
+      lcd.setCursor(7, 0);
+      lcd.print( f_thermo );
+      // delay(10);
+      lcd.setCursor(7, 1);
+      lcd.print( f_resd);
+      //  delay(10);
+    } else {
+      lcd.setCursor(7, 0);
+      lcd.print("off");
+      lcd.setCursor(7, 1);
+      lcd.print("off");
+    };
+    if ( f_thermo == error_temp  && alarm_temp) {
+      lcd.setCursor(7, 0);
+      lcd.print("Err");
+      lcd.setCursor(7, 1);
+      lcd.print("Err");
+    };
+
+
+    //Downheating
+    lcd.setCursor(15, 0);
+    lcd.print("\2C");
+    lcd.setCursor(15, 1);
+    lcd.print("\1C");
+    lcd.setCursor(10, 0);
+    lcd.print("D:");
+    lcd.setCursor(10, 1);
+    lcd.print("H:");
+    if (digitalRead(SW4) == HIGH) {
+      lcd.setCursor(12, 0);
+      lcd.print( dh_thermo);
+      //  delay(10);
+      lcd.setCursor(12, 1);
+      lcd.print(  dh_resd);
+      //  delay(10);
+    } else {
+      lcd.setCursor(12, 0);
+      lcd.print("off");
+      lcd.setCursor(12, 1);
+      lcd.print("off");
+    };
+    if (dh_thermo  == error_temp && alarm_temp) {
+      lcd.setCursor(12, 0);
+      lcd.print("Err");
+      lcd.setCursor(12, 1);
+      lcd.print("Err");
+
+      //Downheating
+    };
+
+    // delay(100);
+  };
+};
